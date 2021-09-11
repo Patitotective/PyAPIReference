09ce593dcbc631964732299b5aa109e33b13b0de
Cristobal <cristobalriaga@gmail.com>
2021-09-10 23:04:25 -0500

    Added logo and load file button

--- PyApiReference/main.py
+++ PyApiReference/main.py
@@ -18,12 +18,15 @@ import PREFS
 from importlib.util import spec_from_file_location, module_from_spec
 # PyQt5
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QFileDialog, QPushButton, QGridLayout
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
 	def __init__(self, parent=None):
 		super().__init__()
 
 		self.init_window()
+
+		self.show()
 
 	def init_window(self):
 		self.setWindowTitle("PyApiReference")
@@ -38,12 +41,21 @@ class MainWidget(QWidget):
 
 		self.init_window()
 
-		self.show()
-
 	def init_window(self):
 		self.setLayout(QGridLayout())
 
-		self.load_file()
+		self.main_frame()
+
+	def main_frame(self):
+		logo = QLabel("PyApiReference")
+		logo.setStyleSheet("font-size: 20px; font-weight: bold;")
+		logo.setAlignment(Qt.AlignCenter | Qt.AlignTop)
+
+		load_file_button = QPushButton("Load file")
+		load_file_button.clicked.connect(self.load_file)
+
+		self.layout().addWidget(logo, 0, 0, 1, 0)
+		self.layout().addWidget(load_file_button, 1, 0)		
 
 	def load_file(self):
 		filename_with_path, file_filter = QFileDialog.getOpenFileName(
@@ -54,6 +66,9 @@ class MainWidget(QWidget):
 
 		filename = os.path.basename(filename_with_path)
 
+		if filename == '':
+			return
+
 		# Get spec from file path and load module from spec
 		spec = spec_from_file_location(os.path.splitext(filename)[0], filename)
 		module = module_from_spec(spec)
@@ -63,8 +78,13 @@ class MainWidget(QWidget):
 		members = inspect.getmembers(module)
 		for member in members:
 			if not member[0].startswith("__"):
-				print(member)
-
+				member_type = member[1]
+				if inspect.isclass(member_type):
+					print(f"class {member}")
+				elif inspect.isfunction(member_type): 
+					print(f"Function {member}")
+				else:
+					print(f"Unknown {member}")
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
