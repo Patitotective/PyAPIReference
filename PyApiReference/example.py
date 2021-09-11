766564b4523a5bf4b6a1d131a27ca4b4978bd24e
Dev <devpatel2556@gmail.com>
2021-09-10 21:23:26 -0400

    new changes

--- PyApiReference/example.py
+++ PyApiReference/example.py
@@ -1,3 +1,5 @@
 def hello():
 	pass
-	
+
+class Test:
+	x = 5
--- PyApiReference/main.py
+++ PyApiReference/main.py
@@ -12,12 +12,23 @@ About:
 
 # Libraries
 import inspect
-import PREFS
+from importlib.util import spec_from_file_location, module_from_spec
 
 # Dependencies
-import example
+#import example
 
-members = inspect.getmembers(example)
+
+#Get file path (From GUI in future)
+file_path = input("File Path: ")
+file_name = file_path.split("\\")[-1]
+
+#Get spec from file path and load module from spec
+spec = spec_from_file_location(file_name, rf"{file_path}")
+module = module_from_spec(spec)
+spec.loader.exec_module(module)
+
+#Get non-built in objects 
+members = inspect.getmembers(module)
 for member in members:
 	if not member[0].startswith("__"):
 		print(member)
