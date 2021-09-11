04431e8a4d40c35b5cb65b13de652ae267eb4c37
Cristobal <cristobalriaga@gmail.com>
2021-09-10 21:01:17 -0500

    added gui

--- PyApiReference/main.py
+++ PyApiReference/main.py
@@ -12,23 +12,62 @@ About:
 
 # Libraries
 import inspect
+import os
+import sys
+import PREFS
 from importlib.util import spec_from_file_location, module_from_spec
+# PyQt5
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QFileDialog, QPushButton, QGridLayout
 
-# Dependencies
-#import example
+class MainWindow(QMainWindow):
+	def __init__(self, parent=None):
+		super().__init__()
+
+		self.init_window()
+
+	def init_window(self):
+		self.setWindowTitle("PyApiReference")
+
+		self.main_widget = MainWidget(parent=self)
+		self.setCentralWidget(self.main_widget)
 
 
-#Get file path (From GUI in future)
-file_path = input("File Path: ")
-file_name = file_path.split("\\")[-1]
+class MainWidget(QWidget):
+	def __init__(self, parent=None):
+		super().__init__()
 
-#Get spec from file path and load module from spec
-spec = spec_from_file_location(file_name, rf"{file_path}")
-module = module_from_spec(spec)
-spec.loader.exec_module(module)
+		self.init_window()
 
-#Get non-built in objects 
-members = inspect.getmembers(module)
-for member in members:
-	if not member[0].startswith("__"):
-		print(member)
+		self.show()
+
+	def init_window(self):
+		self.setLayout(QGridLayout())
+
+		self.load_file()
+
+	def load_file(self):
+		filename_with_path, file_filter = QFileDialog.getOpenFileName(
+			parent=self, 
+			caption="Select a file", 
+			directory=os.path.basename(os.getcwd()), 
+			filter="Python files (*.py)")
+
+		filename = os.path.basename(filename_with_path)
+
+		# Get spec from file path and load module from spec
+		spec = spec_from_file_location(os.path.splitext(filename)[0], filename)
+		module = module_from_spec(spec)
+		spec.loader.exec_module(module)
+
+		# Get non-built in objects 
+		members = inspect.getmembers(module)
+		for member in members:
+			if not member[0].startswith("__"):
+				print(member)
+
+
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+	main_window = MainWindow()
+
+	sys.exit(app.exec_())
